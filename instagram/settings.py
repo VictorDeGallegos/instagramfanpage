38df913f02653712e363d73a2a9cdb8c00649972
Victor Gallegos <316160456@ciencias.unam.mx>
2022-01-12 16:30:58 -0600

    Debug true

--- instagram/settings.py
+++ instagram/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lz_jf*8@+hpu6v3-h1520n33zc_z&$*kf858j@62u*&)8ft5ap'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
