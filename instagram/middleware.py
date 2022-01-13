735eeb7e8f77e450e5aae5aa0f47bbcce7ced097
Victor Gallegos <316160456@ciencias.unam.mx>
2022-01-12 22:50:17 -0600

    Modificando middleware.py sin exito

--- instagram/middleware.py
+++ instagram/middleware.py
@@ -22,7 +22,7 @@ class ProfileCompletionMiddleware:
             if not request.user.is_staff:
                 profile = request.user.profile
                 if not profile.picture or not profile.biography:
-                    if request.path not in [reverse('users:update_profile'), reverse('users:logout')]:
+                    if request.path not in [reverse('update_profile'), reverse('logout')]:
                         return redirect('users:update_profile')
 
         response = self.get_response(request)
